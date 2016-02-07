292be71552056c1bcbcff600cfbcf4412ab4e813
David Björklund <david.bjorklund@gmail.com>
2016-02-07 23:29:44 +0100

    more benchmark

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -4,17 +4,38 @@ import fs from 'fs';
 const input = fs.readFileSync(__dirname + '/../../benchmark/example.html', 'utf8');
 
 if (console.profile) {
-  console.profile('benchmark');
+  console.profile('parse');
 }
-const start = new Date();
+let start = new Date();
 let count = 0;
 while (new Date() - start < 2000) {
   count++;
   queryDom(input);
 }
-const diff = (new Date() - start) / count;
+let diff = (new Date() - start) / count;
 if (console.profileEnd) {
-  console.profileEnd('benchmark');
+  console.profileEnd('parse');
 }
 
 console.log(`parse: ${diff} ms`);
+
+const dom = queryDom(`<div>${input}</div>`);
+
+if (console.profile) {
+  console.profile('getElementsByTagName');
+}
+
+start = new Date();
+count = 0;
+while (new Date() - start < 2000) {
+  for (let i = 0; i < 1000; ++i) {
+    count++;
+    dom[0].getElementsByTagName('div').length
+  }
+}
+diff = (new Date() - start) / count;
+if (console.profileEnd) {
+  console.profileEnd('getElementsByTagName');
+}
+
+console.log(`getElementsByTagName: ${diff} ms`);
