82407b84c51c05b4091f008b6598440504feb3db
David Björklund <david.bjorklund@gmail.com>
2016-04-07 08:40:16 +0200

    update to ava@0.14

--- test.js
+++ test.js
@@ -6,42 +6,50 @@ import tsml from 'tsml';
 test('tagNames & nodeNames are upper case', t => {
   const actual = queryDom('<div></div><DIV></DIV>');
 
-  t.same(actual.length, 2);
-  t.same(actual[0].tagName, 'div');
-  t.same(actual[0].nodeName, 'div');
-  t.same(actual[1].tagName, 'div');
-  t.same(actual[1].nodeName, 'div');
+  t.is(actual.length, 2);
+  t.is(actual[0].tagName, 'div');
+  t.is(actual[0].nodeName, 'div');
+  t.is(actual[1].tagName, 'div');
+  t.is(actual[1].nodeName, 'div');
 });
 
 test('nested has correct tagNames & nodeNames', t => {
   const actual = queryDom('<div><span></span></div>');
 
-  t.same(actual.length, 1);
-  t.same(actual[0].tagName, 'div');
-  t.same(actual[0].nodeName, 'div');
+  t.is(actual.length, 1);
+  t.is(actual[0].tagName, 'div');
+  t.is(actual[0].nodeName, 'div');
 
-  t.same(actual[0].childNodes[0].tagName, 'span');
-  t.same(actual[0].childNodes[0].nodeName, 'span');
+  t.is(actual[0].childNodes[0].tagName, 'span');
+  t.is(actual[0].childNodes[0].nodeName, 'span');
 });
 
 test('getAttribute()', t => {
   const actual = queryDom('<div foo="bar"></div>');
-  t.same(actual[0].getAttribute('does-not-exists'), null,
-    'none existing attribute');
-  t.same(actual[0].getAttribute('does-not-exists'), null,
-    'none existing attribute (cached)');
-  t.same(actual[0].getAttribute('foo'), 'bar', 'existing attribute');
-  t.same(actual[0].getAttribute('foo'), 'bar', 'existing attribute (cached)');
+  t.is(actual[0].getAttribute('does-not-exists'), null, 'none existing attribute');
+  t.is(
+    actual[0].getAttribute('does-not-exists'),
+    null,
+    'none existing attribute (cached)'
+  );
+  t.is(actual[0].getAttribute('foo'), 'bar', 'existing attribute');
+  t.is(actual[0].getAttribute('foo'), 'bar', 'existing attribute (cached)');
 });
 
 test('hasAttribute()', t => {
   const actual = queryDom('<div foo="bar"></div>');
-  t.same(actual[0].hasAttribute('does-not-exists'), false,
-    'none existing attribute');
-  t.same(actual[0].hasAttribute('does-not-exists'), false,
-    'none existing attribute (cached)');
-  t.same(actual[0].hasAttribute('foo'), true, 'existing attribute');
-  t.same(actual[0].hasAttribute('foo'), true, 'existing attribute (cached)');
+  t.is(
+    actual[0].hasAttribute('does-not-exists'),
+    false,
+    'none existing attribute'
+  );
+  t.is(
+    actual[0].hasAttribute('does-not-exists'),
+    false,
+    'none existing attribute (cached)'
+  );
+  t.is(actual[0].hasAttribute('foo'), true, 'existing attribute');
+  t.is(actual[0].hasAttribute('foo'), true, 'existing attribute (cached)');
 });
 
 test('getElementsByTagName()', t => {
@@ -49,11 +57,11 @@ test('getElementsByTagName()', t => {
     <foo></foo>
     <beep><foo></foo></beep>
   </div>`)[0].getElementsByTagName('foo');
-  t.same(actual.length, 2);
-  t.same(actual[0].tagName, 'foo');
-  t.same(actual[0].parentNode.tagName, 'div');
-  t.same(actual[1].tagName, 'foo');
-  t.same(actual[1].parentNode.tagName, 'beep');
+  t.is(actual.length, 2);
+  t.is(actual[0].tagName, 'foo');
+  t.is(actual[0].parentNode.tagName, 'div');
+  t.is(actual[1].tagName, 'foo');
+  t.is(actual[1].parentNode.tagName, 'beep');
 });
 
 test('getElementsByTagName() tricky', t => {
@@ -61,11 +69,11 @@ test('getElementsByTagName() tricky', t => {
     <beep><foo></foo></beep>
     <foo></foo>
   </div>`)[0].getElementsByTagName('foo');
-  t.same(actual.length, 2);
-  t.same(actual[0].tagName, 'foo');
-  t.same(actual[0].parentNode.tagName, 'beep');
-  t.same(actual[1].tagName, 'foo');
-  t.same(actual[1].parentNode.tagName, 'div');
+  t.is(actual.length, 2);
+  t.is(actual[0].tagName, 'foo');
+  t.is(actual[0].parentNode.tagName, 'beep');
+  t.is(actual[1].tagName, 'foo');
+  t.is(actual[1].parentNode.tagName, 'div');
 });
 
 test('classList.contains()', t => {
@@ -75,23 +83,23 @@ test('classList.contains()', t => {
     <div class="foo bar"></div>
   `);
 
-  t.same(actual.length, 3);
+  t.deepEqual(actual.length, 3);
 
-  t.notOk(actual[0].classList.contains('foo'));
-  t.ok(actual[1].classList.contains('foo'));
-  t.notOk(actual[1].classList.contains('bar'));
-  t.ok(actual[2].classList.contains('foo'));
-  t.ok(actual[2].classList.contains('bar'));
-  t.notOk(actual[2].classList.contains('bas'));
+  t.falsy(actual[0].classList.contains('foo'));
+  t.truthy(actual[1].classList.contains('foo'));
+  t.falsy(actual[1].classList.contains('bar'));
+  t.truthy(actual[2].classList.contains('foo'));
+  t.truthy(actual[2].classList.contains('bar'));
+  t.falsy(actual[2].classList.contains('bas'));
 });
 
 test('classList.contains() whitespace in className', t => {
   const actual = queryDom('<div class="  foo   bar  "/>')[0].classList;
 
-  t.ok(actual.contains('foo'));
-  t.ok(actual.contains('bar'));
-  t.notOk(actual.contains(''));
-  t.notOk(actual.contains(' '));
+  t.truthy(actual.contains('foo'));
+  t.truthy(actual.contains('bar'));
+  t.falsy(actual.contains(''));
+  t.falsy(actual.contains(' '));
 });
 
 test('style - single statement', t => {
@@ -99,7 +107,7 @@ test('style - single statement', t => {
   const expected = {
     fontSize: '14px'
   };
-  t.same(actual, expected);
+  t.deepEqual(actual, expected);
 });
 
 test('style - multiple statements', t => {
@@ -119,19 +127,19 @@ test('style - multiple statements', t => {
     msBorderRadius: '10px',
     borderRadius: '10px'
   };
-  t.same(actual, expected);
+  t.deepEqual(actual, expected);
 });
 
 test('style - no style', t => {
   const actual = queryDom('<div></div>')[0].style;
   const expected = {};
-  t.same(actual, expected);
+  t.deepEqual(actual, expected);
 });
 
 test('style - invalid', t => {
   const actual = queryDom('<div style="foo"></div>')[0].style;
   const expected = {};
-  t.same(actual, expected);
+  t.deepEqual(actual, expected);
 });
 
 test('text element', t => {
